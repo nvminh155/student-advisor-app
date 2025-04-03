4ebe58cb3c80acfdf3a2e7d5cef87b93bec6c14a
nvminh155 <nvmttq@gmail.com>
2025-04-03 20:17:58 +0700

    remove unnecessary info

--- firebase/index.ts
+++ firebase/index.ts
@@ -2,18 +2,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { env } from "@/config/env";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCYvuG-omeeglBxscR_EmLgqZSfrgaY704",
+  apiKey: env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: "student-advisor-app-75a11.firebaseapp.com",
   projectId: "student-advisor-app-75a11",
   storageBucket: "student-advisor-app-75a11.firebasestorage.app",
   messagingSenderId: "331906216455",
-  appId: "1:331906216455:web:cb2c0ecca79c29f72570a2",
+  appId: env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
